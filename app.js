c5c0c2306636ba3d503471473aefc0310806c81d
devGeyson <geysonsilvaferreira@gmail.com>
2025-01-30 18:48:51 -0300

    implementação do array

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+let amigos = [];
 
+function adicionarAmigo(){
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome !== ""){
+        amigos.push(nome);
+        
+        atualizarLista();
+        input.value = "";
+    }else{
+        alert("Por favor, Digite um nome válido");
+    }
+}
+
+function atualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(nome =>{
+        let li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    })
+}
